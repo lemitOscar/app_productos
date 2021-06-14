fc8866516a291d1a58e762f65b9f0d36c872cf69
Jess <gogleoscar14@gmail.com>
2021-06-13 19:59:28 -0500

    cambios menores funciona el buscador

--- routes/api.php
+++ routes/api.php
@@ -22,26 +22,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//login
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 //Productos
-Route::get('/listar_productos', [ProductosTable::class, 'listar_productos'])->middleware('auth:sanctum');
+Route::post('/listar_productos', [ProductosTable::class, 'listar_productos'])->middleware('auth:sanctum');
 Route::post('/listar_productos_filtro', [ProductosTable::class, 'listar_productos_filtro'])->middleware('auth:sanctum');
 Route::post('/guardar_productos', [ProductosEdit::class, 'guardar_productos']);
 //Route::post('/actualizar_productos', [ProductosEdit::class, 'actualizar_productos']);
 Route::delete('eliminar_productos', [ProductosEdit::class, 'eliminar_productos']);
-
-
 //Clientes
 Route::get('/listar_clientes', [CustomersTable::class, 'listar_clientes']);
 Route::post('/guardar_clientes', [CustomersEdit::class, 'guardar_clientes']);
 Route::put('/actualizar_clientes', [CustomersEdit::class, 'actualizar_clientes']);
 Route::delete('eliminar_clientes', [CustomersEdit::class, 'eliminar_clientes']);
-
-
 //Almacenes
-
 Route::get('/listar_almacenes', [StocksTable::class, 'listar_almacenes']);
 Route::post('/guardar_almacenes', [StocksEdit::class, 'guardar_almacenes']);
 Route::put('/actualizar_almacenes', [StocksEdit::class, 'actualizar_almacenes']);
